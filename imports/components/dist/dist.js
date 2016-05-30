6474df52b47197b966f7f12ab8749f73390757f0
dcs <dcstrandberg@gmail.com>
2016-05-30 16:25:39 -0500

    Finally I got the distances helper to properly display the distances to the other users rather than their location information. Learned that Template Helper functions can only return a MongoDB cursor. Currently it's all very roundabout: We write each users location to the server, pull that to populate a distances list array as a property of the current user's object in the database. Eventually this will need to be run on a server or something as well as add user authentication. Then you won't be able to access the other users' geo info unless you're that user.

--- imports/components/dist/dist.js
+++ imports/components/dist/dist.js
@@ -26,7 +26,7 @@ class DistCtrl {
             //Use the service to clear the geo watch
             distService.clearGeo(this.watchID);
             vm.on = false;
-            vm.error = distService.errMsg;            
+            vm.error = this.getReactively('distService.errMsg');            
 
         };
         this.compassOn = function() {
@@ -36,13 +36,13 @@ class DistCtrl {
             
             //Update variables
             vm.on = true;
-            vm.error = distService.errMsg;            
+            vm.error = this.getReactively('distService.errMsg');            
         };
-
         
         this.helpers({
             me() { 
-                if (Locations.findOne({name: vm.name}) && vm.on) {
+                if (Locations.findOne({name: vm.name}) && 
+                    vm.getReactively('on')) {
                     return Locations.findOne({name: vm.name});
                 } else {
                     return {
@@ -54,45 +54,24 @@ class DistCtrl {
                     };
                 }
             },
-            locations() { //Should return distance and not lat/long - Eventually the distance calculations will be performed on the server, which should be more secure...
+            locations() { //Should return distance and not lat/long - Eventually the distance calculations will be performed on the server, which should be more secure...??
                 return Locations.find({
                     'name': {
                         $ne: vm.name
                     }
                 });
-            }
-            
-            
+            },
+            distances() {//Use the Locations helper and the Me helper to compute distances
+                if (vm.getReactively('on')) {
+                    return Locations.find({
+                        'name': vm.name
+                        });
+                        
+                } else {
+                    return;
+                }
+            }          
         });
-
-        /*this.computeDist = function(locObj) {
-            //enclosed function to accept the geolocation object and update the display accordingly
-            const EARTHRADIUS = 6371000; //Meters
-            const HOMELAT = 44.940601, HOMELONG = -93.156662;
-            var dist, lat0, long0;  
-                     
-            //Get the location coordinates
-            lat0 = locObj.coords.latitude;
-            long0 = locObj.coords.longitude;
-        
-            //Get the differences in lat and long and convert to radians
-            var deltaLat = (HOMELAT - lat0) * Math.PI / 180;
-            var deltaLong = (HOMELONG - long0) * Math.PI / 180;
-        
-            var a = 
-                0.5 - Math.cos(deltaLat)/2 + 
-                Math.cos(lat0 * Math.PI / 180) * Math.cos(HOMELAT * Math.PI / 180) * 
-                (1 - Math.cos(deltaLong))/2;
-            //Round the distance to 1 decimal point
-            var dist = 
-                (EARTHRADIUS * 2 * Math.asin( Math.sqrt(a) ) ).toFixed(1);
-            //Add the units
-            return dist + " m";
-        };
-        
-        this.onError = (err) => {
-            return "Error(" + err.code + "): " + err.message;
-        };*/
     }
 }
 
--- imports/services/distService.js
+++ imports/services/distService.js
@@ -1,8 +1,7 @@
-//import '../../client/main.js';
-
 import { Locations } from '../api/locations.js';
 
 function DistService() {
+    var vm = this;
         var name; //Maybe the USERID would be better
         var distSettings = { //Settings for grabbing the geolocation obj
             enableHighAccuracy: true,
@@ -38,6 +37,8 @@ function DistService() {
             navigator.geolocation.clearWatch(watchID);
         };        
         
+        
+        
         //CALLBACK FUNCTIONS
         function geoLog(locObj) {
             //Can we talk to the database inside this service?! We'll find out
@@ -66,7 +67,8 @@ function DistService() {
                         'updated': new Date()
                     }
                 });
-            } 
+            }
+            createDistList(); 
             return "";  
         }
         
@@ -77,8 +79,60 @@ function DistService() {
             console.log("Error registered in callback function:" + err.code + ": " + err.message);
             this.errMsg = "Error registered in callback function:" + err.code + ": " + err.message;
         }
-return this;
+
+        //ORIGINALLY THIS WAS GOING TO JUST BE A NON OBJECT FUNCTION, BUT SINCE IT NEEDS TO BE CALLED FROM THE COMPONENT, FOR NOW IT WILL BE MADE INTO A METHOD.
+        this.computeDist = function (myGeo, userGeo) {
+            //enclosed function to accept the geolocation object and update the display accordingly
+            const EARTHRADIUS = 6371000; //Meters
+            var myLat = myGeo.lat, myLong = myGeo.long;
+            //var HOMELAT = 42, HOMELONG = -93;
+            
+            var dist;  
+                     
+            //Get the location coordinates
+            var lat0 = userGeo.lat;
+            var long0 = userGeo.long;
+        
+            //Get the differences in lat and long and convert to radians
+            var deltaLat = (myLat - lat0) * Math.PI / 180;
+            var deltaLong = (myLong - long0) * Math.PI / 180;
+        
+            var a = 
+                0.5 - Math.cos(deltaLat)/2 + 
+                Math.cos(lat0 * Math.PI / 180) * Math.cos(myLat * Math.PI / 180) * (1 - Math.cos(deltaLong))/2;
+            //Round the distance to 1 decimal point
+            var dist = 
+                (EARTHRADIUS * 2 * Math.asin( Math.sqrt(a) ) ).toFixed(1);
+            //Add the units
+            return dist + " m";
+        };
+        function createDistList() {
+            var distList = [], i; 
+            var myEntry = Locations.findOne({"name": name});
+            var userArray = Locations.find({
+                'name': {
+                    $ne: name
+                }
+            });
+            userArray.forEach(function(user) {
+                distList.push({
+                    'name': user.name,
+                    'distance': vm.computeDist(myEntry.geo, user.geo)
+                });
+            });
+            Locations.update({
+                "_id": myEntry._id
+            }, {
+                //Should change this to only update the entries in the distList that need to be updated
+                $set: {
+                    'distList': distList
+                }
+            });
+            return;
+        }
+        
+    //Return the service object
+    return this;
 }
 
-export default DistService; /* angular.module('dist')
-    .factory('distService', DistService);*/
+export default DistService; 
